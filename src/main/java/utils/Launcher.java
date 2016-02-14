39dca0fe8da0d553272ad551eddfeefcb0e9dc25
Alex Baggins <loki_ord@rambler.ru>
2016-02-14 23:36:32 +0300

    Null value + Scopes

--- src/main/java/utils/Launcher.java
+++ src/main/java/utils/Launcher.java
@@ -10,13 +10,24 @@ public class Launcher {
         ApplicationContext appContext = new ClassPathXmlApplicationContext("spring-context.xml");
 
         Occupation occ = (Occupation) appContext.getBean("occupation");
+        Occupation occ1 = (Occupation) appContext.getBean("occupation1");
+        Occupation occ2 = (Occupation) appContext.getBean("occupation2");
         Employer employer = (Employer) appContext.getBean("employer");
+        Employer employer_proto1 = (Employer) appContext.getBean("employer");
+        Employer employer_proto2 = (Employer) appContext.getBean("employer");
         Employer employer1 = (Employer) appContext.getBean("employer1");
         Employer employer2 = (Employer) appContext.getBean("employer2");
 
-        System.out.println("occ = " + occ);
+        employer_proto1.setName("Proto_1");
+        employer_proto2.setEmail("ProtoEmail_2");
+
+//        System.out.println("occ = " + occ);
+//        System.out.println("occ1 = " + occ1);
+//        System.out.println("occ2 = " + occ2);
         System.out.println("employer = " + employer);
-        System.out.println("employer1 = " + employer1);
-        System.out.println("employer2 = " + employer2);
+        System.out.println("employer_proto1 = " + employer_proto1);
+        System.out.println("employer_proto2 = " + employer_proto2);
+//        System.out.println("employer1 = " + employer1);
+//        System.out.println("employer2 = " + employer2);
     }
 }
